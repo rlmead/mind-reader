53d7cf86528a9f5399b2096dd6d212cbf27c0f66
Rebecca Mead <beckyleemead@gmail.com>
2020-09-23 18:01:58 -0400

    beginnings of connecting js to html

--- js/data.js
+++ js/data.js
@@ -2,36 +2,37 @@
 const states = [
     {
         "main_text": "I can read your mind!",
-        "round_button": "Go"
+        "round_button": "fas fa-arrow-alt-circle-right"
     },
     {
         "main_text": "Pick a number from 01 to 99.",
         "rectangle_button": "Next",
         "extra_text": "Click \"next\" after you've chosen your number.",
-        "round_button": "Start Over"
+        "round_button": "fa fa-redo-alt"
     },
     {
         "main_text": "Add both digits together to get a new number.",
         "rectangle_button": "Next",
         "extra_text": "For example, 14: 1 + 4 = 5\nClick \"next\" to proceed.",
-        "round_button": "Start Over"
+        "round_button": "fa fa-redo-alt"
     },
     {
         "main_text": "Subtract your new number from your original number.",
         "rectangle_button": "Next",
         "extra_text": "For example: 14 - 5 = 9\nClick \"next\" to proceed.",
-        "round_button": "Start Over"
+        "round_button": "fa fa-redo-alt"
     },
     {
         "rectangle_button": "Reveal",
         "extra_text": "Scroll to find your new number, and note the symbol beside it.",
-        "round_button": "Start Over"
+        "round_button": "fa fa-redo-alt"
     },
     {
         "main_text": "&",
         "extra_text": "Your symbol is &",
-        "round_button": "Start Over"
+        "round_button": "fa fa-redo-alt"
     }
 ]
 
 // define object with html info for each of the 4 possible html elements per state?
+// deciding against this, not sure how the js will parse the html stored as json
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // constants for accessing document elements
-
+const main_text = document.getElementById('main_text');
+const rectangle_button = document.getElementById('rectangle_button');
+const extra_text = document.getElementById('extra_text');
+const round_button = document.getElementById('round_button');
 
 // function to change main document element state
 
@@ -8,6 +11,9 @@
 // function to populate html elements according to current state and data in states object
 
 
+// these should be helpful:
+// element.classList.remove("mystyle");
+// element.classList.add("mystyle");
 
 // trigger state change according to keypress or round button press
 

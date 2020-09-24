6aa19f3efc92bd6d1d6a87da1f10baccfa06a937
Rebecca Mead <beckyleemead@gmail.com>
2020-09-24 14:58:49 -0400

    change redo icon to undo icon

--- js/data.js
+++ js/data.js
@@ -8,28 +8,28 @@ const content = [
         "main_text": "Pick a number from 1 to 99.",
         "rectangle_button": "Next",
         "extra_text": "Click \"next\" after you've chosen your number.",
-        "icon": "fa-redo"
+        "icon": "fa-undo"
     },
     {
         "main_text": "Add both digits together to get a new number.",
         "rectangle_button": "Next",
         "extra_text": "For example, 14: 1 + 4 = 5. Click \"next\" to proceed.",
-        "icon": "fa-redo"
+        "icon": "fa-undo"
     },
     {
         "main_text": "Subtract your new number from your original number.",
         "rectangle_button": "Next",
         "extra_text": "For example: 14 - 5 = 9. Click \"next\" to proceed.",
-        "icon": "fa-redo"
+        "icon": "fa-undo"
     },
     {
         "rectangle_button": "Reveal",
         "extra_text": "Scroll to find your new number, and note the symbol beside it.",
-        "icon": "fa-redo"
+        "icon": "fa-undo"
     },
     {
         "main_text": "&",
         "extra_text": "Your symbol is &",
-        "icon": "fa-redo"
+        "icon": "fa-undo"
     }
 ]

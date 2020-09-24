9eee2d85c46a229649671589fbb51447e5577354
Rebecca Mead <beckyleemead@gmail.com>
2020-09-24 13:49:21 -0400

    fixing fontawesome icons

--- js/data.js
+++ js/data.js
@@ -2,34 +2,34 @@
 const content = [
     {
         "main_text": "I can read your mind!",
-        "round_button": "fas fa-arrow-alt-circle-right"
+        "round_button_icon": "fa-arrow-right"
     },
     {
         "main_text": "Pick a number from 1 to 99.",
         "rectangle_button": "Next",
         "extra_text": "Click \"next\" after you've chosen your number.",
-        "round_button": "fa fa-redo-alt"
+        "round_button_icon": "fa-redo"
     },
     {
         "main_text": "Add both digits together to get a new number.",
         "rectangle_button": "Next",
         "extra_text": "For example, 14: 1 + 4 = 5. Click \"next\" to proceed.",
-        "round_button": "fa fa-redo-alt"
+        "round_button_icon": "fa-redo"
     },
     {
         "main_text": "Subtract your new number from your original number.",
         "rectangle_button": "Next",
         "extra_text": "For example: 14 - 5 = 9\nClick \"next\" to proceed.",
-        "round_button": "fa fa-redo-alt"
+        "round_button_icon": "fa-redo"
     },
     {
         "rectangle_button": "Reveal",
         "extra_text": "Scroll to find your new number, and note the symbol beside it.",
-        "round_button": "fa fa-redo-alt"
+        "round_button_icon": "fa-redo"
     },
     {
         "main_text": "&",
         "extra_text": "Your symbol is &",
-        "round_button": "fa fa-redo-alt"
+        "round_button_icon": "fa-redo"
     }
 ]
